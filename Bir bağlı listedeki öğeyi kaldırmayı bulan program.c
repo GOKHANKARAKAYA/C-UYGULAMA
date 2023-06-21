97670a25431230c6afd7555927408800b575cf9f
Gökhan <126273014+GOKHANKARAKAYA@users.noreply.github.com>
2023-06-21 14:05:05 +0300

    Update Bir bağlı listedeki öğeyi kaldırmayı bulan program.c

--- Bir bağlı listedeki öğeyi kaldırmayı bulan program.c
+++ Bir bağlı listedeki öğeyi kaldırmayı bulan program.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+int dizi[20],boyut;
+printf("Olusturmak istediginiz dizinin eleman sayisini giriniz:");
+scanf("%d",&boyut);
+for(int i=0;i<boyut;i++){
+    printf("Dizinin %d. elemani:",i+1);
+    scanf("%d",&dizi[i]);
+}
+int e[100]={0};
+for(int i=0;i<boyut;i++){
+    for(int j=0;j<boyut;j++){
+        if(dizi[i]==dizi[j]){
+            e[i]+=1;
+        }
+    }
+}
+int buyuk,sayac=0;
+for(int i=0;i<boyut;i++){
+for(int j=0;j<boyut;j++){
+if(e[i]>=e[j]){
+    sayac=1;
+}
+else{
+    sayac=0;
+}
+}
+if(sayac==1){
+buyuk=i;
+break;
+}
+sayac=0;
+}
+printf("Dizide max tekrar eden karakter =%d",dizi[buyuk]);
+    return 0;
+}
